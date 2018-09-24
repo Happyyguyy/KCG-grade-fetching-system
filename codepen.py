60b5c49fdf789d424a24d142cc4c02cea47ea168
happyyguyy <chodr@bc.edu>
2018-09-24 12:37:43 +0100

    Changing from web to client based system instead

--- codepen.py
+++ codepen.py
@@ -1,16 +1,13 @@
-from csv import DictReader, DictWriter
+import sqlite3
 
-csv = []
+db = sqlite3.connect("db.db")
+cur = db.cursor()
 
+cur.execute("DROP TABLE test")
+cur.execute("CREATE TABLE test (val1 INT PRIMARY KEY NOT NULL, val2 INT)")
+cur.execute("INSERT INTO test (val1,val2) VALUES ('e',4);")
+cur.execute("INSERT INTO test (val1,val2) VALUES (2,4);")
+db.commit()
 
-with open("leglist.csv") as file:
-    dict_reader = DictReader(file)
-    with open("leglist_new.csv", "w") as write_file:
-        fieldnames = ["title", "name", "district", "party", "phone_num", "email", "leg_page", "last", "first", "img_link", "id"]
-        writer = DictWriter(write_file, fieldnames=fieldnames, delimiter=",", lineterminator="\n")
-        writer.writeheader()
-
-        for row in dict_reader:
-            id = row["title"][:3].lower() + row["last"].lower() + row["district"]
-            writer.writerow({"title":row['title'], "name": row["first"] + " " + row["last"], "district": row["district"], "party": row["party"], "phone_num": row["phone_num"], "email": row["email"], "leg_page": row["link"], "last": row["last"], "first": row["first"], "img_link": row["img_link"], "id": id})
-# print(csv)
+for row in cur.execute("SELECT * FROM test"):
+    print(row)
